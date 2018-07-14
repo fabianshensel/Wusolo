f6c204a387b1909da8bde1fb624c9893b231a85b
I_am_secret <to.glass@t-online.de>
2018-07-14 18:47:09 +0200

    dummy Class Player,PlayerStats

--- app/src/main/java/com/example/fabia/doppelkopfnew/Player.java
+++ app/src/main/java/com/example/fabia/doppelkopfnew/Player.java
@@ -1,6 +1,10 @@
 package com.example.fabia.doppelkopfnew;
 
+import android.media.Image;
+
 public class Player {
     private String name;
+    private String comment;
+    private Image image;
     private PlayerStats stats;
 }
