e5034b2e6294e01ef424426f939c1f68d14cbe20
fabianshensel <40912667+fabianshensel@users.noreply.github.com>
2018-07-16 20:05:57 +0200

    Konnte mein Problem lösen. Die startet jetzt wieder.

--- app/src/main/java/com/example/fabia/doppelkopfnew/MenuActivity.java
+++ app/src/main/java/com/example/fabia/doppelkopfnew/MenuActivity.java
@@ -2,6 +2,7 @@ package com.example.fabia.doppelkopfnew;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -15,10 +16,10 @@ public class MenuActivity extends AppCompatActivity {
     private Toolbar mToolbar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+/*
         mToolbar = findViewById(R.id.nav_action);
         setSupportActionBar(mToolbar);
 
@@ -27,7 +28,9 @@ public class MenuActivity extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+       getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+       */
     }
 
     @Override
