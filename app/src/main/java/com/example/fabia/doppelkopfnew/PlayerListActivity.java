ff91dc0d368ec79cf867ef4ca9eec39d602bdfdb
I_am_secret <to.glass@t-online.de>
2018-07-17 20:28:32 +0200

    ....

--- app/src/main/java/com/example/fabia/doppelkopfnew/PlayerListActivity.java
+++ app/src/main/java/com/example/fabia/doppelkopfnew/PlayerListActivity.java
@@ -1,6 +1,5 @@
 package com.example.fabia.doppelkopfnew;
 
-import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,9 +7,12 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class PlayerListActivity extends AppCompatActivity {
+
+    ArrayList<Player> playerList = new ArrayList<>();
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
@@ -19,12 +21,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_player_list);
 
+        //DrawerMenuLayout einrichten
         mToolbar = findViewById(R.id.nav_action);
         setSupportActionBar(mToolbar);
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.myDrawer);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.myPlayerDrawer);
         mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
@@ -32,24 +35,18 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+
+        fillList();
+
+
     }
 
-    public void startAktivityMain(View v) {
-        startActivity(new Intent(MainActivity.this,MenuActivity.class));
-    }
-
+    //Menu einrichten
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_playerlist, menu);
         return true;
-    }
-
-
-
-
-    public void onClickButton(MenuItem item){
-        System.out.println("Okay");
     }
 
     @Override
@@ -63,26 +60,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //Man muss noch fuer einzelne MenuItems das OnClick event in der Ressource festlegen
-    public boolean onMenuItemSelected(MenuItem item){
-
-        switch (item.getItemId()) {
-            case R.id.nav_about_us:
-                AboutUsActivity aboutUsActivity = new AboutUsActivity();
-                finish();
-                startActivity(new Intent(MainActivity.this,AboutUsActivity.class));
-                finish();
-                aboutUsActivity.getMeOut();
-                return true;
-
-            case R.id.nav_profiles:
-
-                Intent myIntent = new Intent(MainActivity.this,PlayerListActivity.class);
-                startActivity(myIntent);
-
-                return true;
-            default:
-                return true;
-        }
+    private void fillList(){
+        playerList.add(new Player("Fabian Hensel","Ich spiele super gerne Doppelkopf",null,new PlayerStats(1,99)));
+        playerList.add(new Player("Torben Glass","Halt dein Maul",null,new PlayerStats(99,1)));
+        playerList.add(new Player("Jeremy Tuller","Ich hab dann mal Recht studiert",null,new PlayerStats(0,0)));
+        playerList.add(new Player("Simon Wusolo","Wo ist die Milch hin?",null,new PlayerStats(99,99)));
     }
+
 }
